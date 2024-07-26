13e40f6ffd699eb144b0d1a64610f8655202f3ea
Hiernymus <hiernysident@outlook.com>
2024-07-26 20:19:45 +0800

    The first edit

--- pages-code/src/App.jsx
+++ pages-code/src/App.jsx
@@ -9,14 +9,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+        <img src={reactLogo} className="logo react" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hiernymus's Blog</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
