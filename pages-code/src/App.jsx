942d6f96f3ac3e5b1df46ff2884e8e3bd129f10e
Hiernymus <hiernysident@outlook.com>
2024-07-27 11:42:59 +0800

    edit the home page logo

--- pages-code/src/App.jsx
+++ pages-code/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Logo from '/icon.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return (g
     <>
       <div>
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-        <img src={reactLogo} className="logo react" alt="React logo" />
+        <img src={Logo} className="logo" alt="logo" />
       </div>
       <h1>Hiernymus's Blog</h1>
       <div className="card">
